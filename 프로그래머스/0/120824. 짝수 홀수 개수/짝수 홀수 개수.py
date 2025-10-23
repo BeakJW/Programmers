05448c2058410dd4d4039de05a9f73f51ad2a812
백진우 <104333195+BeakJW@users.noreply.github.com>
2025-10-23 18:10:12 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    jjak = 0
+    hol = 0
+    
+    for i in num_list:
+        if i%2==0:
+            jjak +=1
+        else:
+            hol +=1
+        
+    return [jjak,hol]
